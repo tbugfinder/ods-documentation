e8c5bfd89c5c3c532eea2ddfc993530d1c970060
Richard Attermeyer <richard.attermeyer@opitz-consulting.com>
2019-04-15 10:40:45 +0200

    working on travis integration

--- .travis/push.sh
+++ .travis/push.sh
@@ -14,7 +14,7 @@ commit_website_files() {
 
 upload_files() {
   git remote add origin-pages https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git > /dev/null 2>&1
-  git push --quiet --set-upstream origin-pages master
+  git push --all --quiet --set-upstream origin-pages
 }
 
 setup_git
