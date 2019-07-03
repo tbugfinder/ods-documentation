4cc5f72bb6e6df7b84dac2119408ce9dab000eba
Richard Attermeyer <richard.attermeyer@gmail.com>
2019-07-03 08:26:49 +0200

    updated to only push master

--- .travis/push.sh
+++ .travis/push.sh
@@ -17,7 +17,7 @@ commit_website_files() {
 
 upload_files() {
   git remote add origin-pages https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git > /dev/null 2>&1
-  git push --all --quiet --set-upstream origin-pages
+  git push --quiet --set-upstream origin-pages master
 }
 
 setup_git
