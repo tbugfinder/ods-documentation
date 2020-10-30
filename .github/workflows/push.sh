9529a3024faa48b08ae0fd3a42ffa609c4556701
tbugfinder <tbugfinder@online.ms>
2020-10-30 14:36:42 +0100

    switch to autocreated-GITHUB_TOKEN

--- .github/workflows/push.sh
+++ .github/workflows/push.sh
@@ -19,7 +19,7 @@ commit_website_files() {
 }
 
 upload_files() {
-  git remote add origin-pages https://${GH_TOKEN}@github.com/${GITHUB_REPOSITORY}.git > /dev/null 2>&1
+  git remote add origin-pages https://${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git > /dev/null 2>&1
   git push --quiet --set-upstream origin-pages $GITHUB_REF_SLUG
 }
 
